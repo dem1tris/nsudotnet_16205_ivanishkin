71c0a09897bc571c70221d48cf386169b6f388bb
Dmitry Ivanishkin <d@ivanishkin.ru>
2019-05-28 00:28:31 +0700

    task 7 done

---
+++ task7-social/task7-social/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace task7_social {
+    class Program {
+        private static string _serviceToken;
+        private static string _accessToken;
+
+        static void Main(string[] args) {
+            var vk = new VkApiWrapper();
+
+            AskCredentials();
+
+            PrintHelp();
+
+            while (true) {
+                var line = Console.ReadLine();
+                line = line?.Trim();
+                switch (line) {
+                    case "": {
+                        break;
+                    }
+                    case "/f": {
+                        vk.Authorize(_serviceToken);
+                        try {
+                            var friendList = vk.GetFriendList();
+                            Console.WriteLine("dem1tris' friend list:");
+                            foreach (var friend in friendList) {
+                                Console.WriteLine(friend);
+                            }
+                        }
+                        catch (VkNet.Exception.UserAuthorizationFailException) {
+                            Console.WriteLine("Bad credentials (service token). Try again");
+                            AskCredentials();
+                            PrintHelp();
+                        }
+
+                        Console.WriteLine("");
+                        break;
+                    }
+                    case "/h": {
+                        PrintHelp();
+                        break;
+                    }
+                    case "/q": {
+                        return;
+                    }
+                    default: {
+                        try {
+                            vk.Authorize(_accessToken);
+                            vk.SendMessage(line);
+                        }
+                        catch (VkNet.Exception.UserAuthorizationFailException) {
+                            Console.WriteLine("Bad credentials (access token). Try again");
+                            AskCredentials();
+                            PrintHelp();
+                        }
+
+                        break;
+                    }
+                }
+            }
+        }
+
+        private static void AskCredentials() {
+            var repeat = false;
+            do {
+                try {
+                    Console.WriteLine("VK service token:");
+                    _serviceToken = ReadPassword();
+                    Console.WriteLine("VK access token:");
+                    _accessToken = ReadPassword();
+                    repeat = false;
+                }
+                catch (ArgumentException) {
+                    Console.WriteLine("Id should be integer");
+                    repeat = true;
+                }
+            } while (repeat);
+        }
+
+        private static string ReadPassword() {
+            var pass = "";
+            do {
+                var key = Console.ReadKey(true);
+
+                if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter) {
+                    pass += key.KeyChar;
+                } else {
+                    if (key.Key == ConsoleKey.Backspace && pass.Length > 0) {
+                        pass = pass.Substring(0, (pass.Length - 1));
+                    } else if (key.Key == ConsoleKey.Enter) {
+                        break;
+                    }
+                }
+            } while (true);
+
+            return pass;
+        }
+
+        private static void PrintHelp() {
+            Console.WriteLine("Available commands:");
+            Console.WriteLine("/f - Print list of dem1tris' friends");
+            Console.WriteLine("/q - Exit");
+            Console.WriteLine("/h - Show this text again");
+            Console.WriteLine("<some text> - Send message to dem1tris");
+        }
+    }
+}
---
+++ task7-social/task7-social/VkApiWrapper.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+using VkNet;
+using VkNet.AudioBypassService.Extensions;
+using VkNet.Enums.Filters;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
+
+namespace task7_social {
+    internal class VkApiWrapper {
+        private static long _myUserId = 32101335;
+        private readonly VkApi _vk;
+        private readonly Lazy<Random> _lazyRandom;
+
+        public VkApiWrapper() {
+            var services = new ServiceCollection();
+            services.AddAudioBypass();
+            _vk = new VkApi(services);
+            _lazyRandom = new Lazy<Random>();
+        }
+
+        public bool Authorize(string token) {
+            //long? captchaSid = null;
+            //string captchaKey = null;
+            //bool retry = false;
+            //do {
+            try {
+                _vk.Authorize(new ApiAuthParams {
+                    AccessToken = token,
+                    Settings = Settings.All,
+                    //TwoFactorAuthorization = () => {
+                    //    Console.WriteLine("Enter SMS-code:");
+                    //    return Console.ReadLine();
+                    //},
+                    //CaptchaSid = captchaSid,
+                    //CaptchaKey = captchaKey
+                });
+            }
+            catch (VkNet.Exception.VkAuthorizationException) {
+                return false;
+            }
+            //catch (VkNet.Exception.CaptchaNeededException e)
+            //{
+            //    captchaSid = e.Sid;
+            //    Console.WriteLine($"Need to solve the captcha: {e.Img}");
+            //    Console.WriteLine("Enter the answer:");
+            //    captchaKey = Console.ReadLine();
+            //    retry = true;
+            //}
+            //} while (retry);
+
+            return true;
+        }
+
+        public List<string> GetFriendList() {
+            var friends = _vk.Friends.Get(new FriendsGetParams
+                {UserId = _myUserId, Fields = ProfileFields.FirstName | ProfileFields.LastName});
+
+            List<string> friendsStrings = new List<string>(friends.Count);
+            foreach (var friend in friends) {
+                friendsStrings.Add($"{friend.FirstName} {friend.LastName}");
+            }
+
+            return friendsStrings;
+        }
+
+        public void SendMessage(string message) {
+            var @params = new MessagesSendParams {
+
+                RandomId = _lazyRandom.Value.Next(),
+                Message = message,
+                UserId = _myUserId
+            };
+            _vk.Messages.Send(@params);
+        }
+    }
+}
